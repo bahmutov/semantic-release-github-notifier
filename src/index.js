364dcad4a2083f750f3fe2a5e2ca5567f66d57b2
Hutson Betts <hutson@hyper-expanse.net>
2015-12-01 23:23:16 -0600

    test(gitHead): Test error condition for gitHead.

--- src/index.js
+++ src/index.js
@@ -10,12 +10,6 @@ var parseGitHubUrl = require('parse-github-repo-url');
 function githubNotifier(pluginConfig, config, callback) {
   var ghUrl = config.options.githubUrl ? url.parse(config.options.githubUrl) : {};
 
-  var github = new GitHubApi({
-    port: ghUrl.port,
-    protocol: (ghUrl.protocol || '').split(':')[0] || null,
-    host: ghUrl.hostname,
-  });
-
   gitHead(function(err, hash) {
     if (err) {
       return callback(err);
@@ -39,6 +33,12 @@ function githubNotifier(pluginConfig, config, callback) {
     if (config.options.debug && !config.options.githubToken) {
       return callback(null, false, release);
     }
+
+    var github = new GitHubApi({
+      port: ghUrl.port,
+      protocol: (ghUrl.protocol || '').split(':')[0] || null,
+      host: ghUrl.hostname,
+    });
 
     github.authenticate({
       type: 'oauth',
