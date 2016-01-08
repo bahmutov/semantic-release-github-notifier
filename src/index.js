65a860fb4cb20bd9da3e7b6cfbe0e432b4605a86
Hutson Betts <hutson@hyper-expanse.net>
2016-01-07 22:17:23 -0600

    refactor(index): Remove code.

--- src/index.js
+++ src/index.js
@@ -1,56 +1,27 @@
 'use strict';
 
-module.exports = githubNotifier;
-
 var url = require('url');
-var gitHead = require('git-head');
 var GitHubApi = require('github');
-var parseGitHubUrl = require('parse-github-repo-url');
+
+module.exports = githubNotifier;
 
 function githubNotifier(pluginConfig, config, callback) {
   var ghUrl = config.options.githubUrl ? url.parse(config.options.githubUrl) : {};
 
-  gitHead(function(err, hash) {
-    if (err) {
-      return callback(err);
-    }
+  if (config.options.debug && !config.options.githubToken) {
+    return callback(null);
+  }
 
-    var ghRepo = parseGitHubUrl(config.pkg.repository.url);
-
-    // jscs:disable
-    var release = {
-      owner: ghRepo[0],
-      repo: ghRepo[1],
-      name: 'v${pkg.version}',
-      'tag_name': 'v${pkg.version}',
-      'target_commitish': hash,
-      draft: !!config.options.debug,
-      body: config.releaseNotes.log,
-    };
-
-    // jscs:enable
-
-    if (config.options.debug && !config.options.githubToken) {
-      return callback(null, false, release);
-    }
-
-    var github = new GitHubApi({
-      port: ghUrl.port,
-      protocol: (ghUrl.protocol || '').split(':')[0] || null,
-      host: ghUrl.hostname,
-    });
-
-    github.authenticate({
-      type: 'oauth',
-      token: config.options.githubToken,
-    });
-
-    github.releases.createRelease(release, function(err) {
-      if (err) {
-        return callback(err);
-      }
-
-      callback(null, true, release);
-    });
+  var github = new GitHubApi({
+    port: ghUrl.port,
+    protocol: (ghUrl.protocol || '').split(':')[0] || null,
+    host: ghUrl.hostname,
   });
+
+  github.authenticate({
+    type: 'oauth',
+    token: config.options.githubToken,
+  });
+
+  callback(null);
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -9,19 +9,4 @@ describe('semantic-release-github-notifier', function() {
   it('should be a function', function() {
     expect(plugin).to.be.a('function');
   });
-
-  it('should return an error if directory is not a git repo', function(done) {
-    var tmp = require('tmp');
-
-    var currentDirectory = process.cwd();
-    tmp.dir(function(err, path) {
-      process.chdir(path);
-
-      plugin({}, { options: { githubUrl: 'test/example' } }, function(err) {
-        expect(err).to.be.defined;
-        process.chdir(currentDirectory);
-        done();
-      });
-    });
-  });
 });
