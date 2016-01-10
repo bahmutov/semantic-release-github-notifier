37ab54338e27c7e2fa16653359f36e8d93e0c8bd
Hutson Betts <hutson@hyper-expanse.net>
2016-01-10 09:22:10 -0600

    refactor(index): Restructure to support parsing git commits.

--- src/index.js
+++ src/index.js
@@ -2,26 +2,39 @@
 
 var url = require('url');
 var GitHubApi = require('github');
+var parser = require('conventional-commits-parser');
+var through = require('through2');
 
 module.exports = githubNotifier;
 
 function githubNotifier(pluginConfig, config, callback) {
-  var ghUrl = config.options.githubUrl ? url.parse(config.options.githubUrl) : {};
-
-  if (config.options.debug && !config.options.githubToken) {
-    return callback(null);
-  }
+  var githubConfig = config.options.githubUrl ? url.parse(config.options.githubUrl) : {};
 
   var github = new GitHubApi({
-    port: ghUrl.port,
-    protocol: (ghUrl.protocol || '').split(':')[0] || null,
-    host: ghUrl.hostname,
+    version: '3.0.0',
+    port: githubConfig.port,
+    protocol: (githubConfig.protocol || '').split(':')[0] || null,
+    host: githubConfig.hostname,
   });
 
-  github.authenticate({
-    type: 'oauth',
-    token: config.options.githubToken,
-  });
+  if (config.options.debug) {
+    github.authenticate({
+      type: 'oauth',
+      token: config.options.githubToken,
+    });
+  }
 
-  callback(null);
+  parser()
+    .pipe(through.obj(function(commit, enc, cb) {
+      if (config.options.debug) {
+        return callback();
+      }
+
+      // post to github.
+
+      cb();
+    }))
+    .on('end', function() {
+      callback();
+    });
 }
